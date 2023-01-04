97892e82b4e1e613e5a8b14378b047de20275784
L. Coelho <coelho.le@protonmail.com>
2023-01-04 00:50:05 -0300

    ;;
    ;;

--- src/lib/geom.pas
+++ src/lib/geom.pas
@@ -3,6 +3,8 @@ unit geom;
 interface
 
 uses
+  Windows,
+
   Types;
 
 type
@@ -14,8 +16,9 @@ type
     bottom
       :longint;
   end;
-
   RECT=tagRECT;
+  _RECT=RECT;
+  RECTL=_RECT;
 
   Dimension2D=class(TInterfacedObject)
   private
@@ -25,9 +28,12 @@ type
     constructor Create(const width,height : longint);overload;
     constructor Create(const d:Dimension2D);overload;
     function Equals(obj:TObject):boolean;
+    function ToString:string;
     property Width:longint read fSize.cx write fSize.cx;
     property Height:longint read fSize.cy write fSize.cy;
     property Value:tagSIZE read fSize;
+    procedure Resize(const newWidth,newHeight:integer);overload;
+    procedure Resize(const newSize:Dimension2D);overload;
   end;
 
   Point2D=class(TInterfacedObject)
@@ -38,21 +44,94 @@ type
     constructor Create(const x,y:longint);overload;
     constructor Create(const p:Point2D);overload;
     function Equals(obj:TObject):boolean;
+    function ToString:string;
     property X:longint read fPoint.X write fPoint.X;
     property Y:longint read fPoint.Y write fPoint.Y;
     property Value:tagPoint read fPoint;
-    procedure Move(const x,y:longint);overload;
-    procedure Move(const p:Point2D);overload;
+    procedure MoveTo(const x,y:longint);overload;
+    procedure MoveTo(const p:Point2D);overload;
     procedure Translate(const dx,dy:longint);overload;
     procedure Translate(const d:Dimension2D);overload;
     class function Distance(const p1,p2:Point2D):extended;overload;
     function Distance(const p:Point2D):extended;overload;
   end;
 
+  Rectangle=class(TInterfacedObject)
+  private
+    fRect:tagRECT;
+    function GetTopLeft:Point2D;
+    function GetBottomLeft:Point2D;
+    function GetTopRight:Point2D;
+    function GetBottomRight:Point2D;
+    function GetSize:Dimension2D;
+    function GetDiagonalLength:extended;
+    function IsEmpty:boolean;
+    function IsSquare:boolean;
+  public
+    constructor Create;overload;
+    constructor Create(const x1,y1,x2,y2:longint);overload;
+    constructor Create(const p1,p2:Point2D);overload;
+    constructor Create(const origin:Point2D; const size:Dimension2D);overload;
+    constructor Create(const cx,cy:longint);overload;
+    constructor Create(const size:Dimension2D);overload;
+    constructor Create(const p:Point2D);overload;
+    function Equals(obj:TObject):boolean;
+    function ToString:string;
+    property Top:longint read fRect.top;
+    property Left:longint read fRect.left;
+    property Bottom:longint read fRect.bottom;
+    property Right:longint read fRect.right;
+    property TopLeft:Point2D read GetTopLeft;
+    property BottomLeft:Point2D read GetBottomLeft;
+    property TopRight:Point2D read GetTopRight;
+    property BottomRight:Point2D read GetBottomRight;
+    property Value:tagRECT read fRect;
+    property Size:Dimension2D read GetSize;
+    property DiagonalLength:extended read GetDiagonalLength;
+    function HasWidth:boolean;
+    function HasHeight:boolean;
+    property Empty:boolean read IsEmpty;
+    property Square:boolean read IsSquare;
+    procedure Inflate(const dx,dy:integer);//v. https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-inflaterect
+    procedure Resize(const newWidth,newHeight:integer);overload;
+    procedure Resize(const newSize:Dimension2D);overload;
+    procedure Offset(const dx,dy:integer);//v. https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-offsetrect
+    procedure MoveTo(const x,y:integer);overload;
+    procedure MoveTo(const p:Point2D);overload;
+    function PtInRect(const p:Point2D):boolean;overload;//v. https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-ptinrect
+    function PtInRect(const x,y:integer):boolean;overload;
+  end;
+
+function SizeToString(const size:tagSIZE):string;
+function PointToString(const point:tagPOINT):string;
+function RectToString(const rect:tagRECT):string;overload;
+function RectToString(const rect:TRect):string;overload;
+
 implementation
 
 uses
+  Math,
   SysUtils;
+
+function SizeToString(const size:tagSIZE):string;
+begin
+  result:='[cx='+IntToStr(size.cx)+',cy='+IntToStr(size.cy)+']'
+end;
+
+function PointToString(const point:tagPOINT):string;
+begin
+  result:='[X='+IntToStr(point.X)+',Y='+IntToStr(point.Y)+']';
+end;
+
+function RectToString(const rect:tagRECT):string;
+begin
+  result:='[TopLeft=('+IntToStr(rect.left)+','+IntToStr(rect.top)+'),BottomRight=('+IntToStr(rect.right)+','+IntToStr(rect.bottom)+')]';
+end;
+
+function RectToString(const rect:TRect):string;
+begin
+  result:='[TopLeft=('+IntToStr(rect.left)+','+IntToStr(rect.top)+'),BottomRight=('+IntToStr(rect.right)+','+IntToStr(rect.bottom)+')]';
+end;
 
 constructor Dimension2D.Create;
 begin
@@ -87,6 +166,25 @@ begin
     result:=FALSE
   else
     result:=((obj as Dimension2D).fSize.cx=self.fSize.cx) and ((obj as Dimension2D).fSize.cy=self.fSize.cy);
+end;
+
+function Dimension2D.ToString:string;
+begin
+  result:='geom.Dimension2D'+SizeToString(self.fSize);
+end;
+
+procedure Dimension2D.Resize(const newWidth,newHeight:integer);
+begin
+  self.fSize.cx:=newWidth;
+  self.fSize.cy:=newHeight;
+end;
+
+procedure Dimension2D.Resize(const newSize:Dimension2D);
+begin
+  if newSize=NIL then
+    Exception.Create('geom.Dimension2D.Resize(Dimension2D): Nil pointer');
+
+  self.Resize(newSize.Width,newSize.Height);
 end;
 
 constructor Point2D.Create;
@@ -124,13 +222,18 @@ begin
     result:=((obj as Point2D).fPoint.X=self.fPoint.X) and ((obj as Point2D).fPoint.Y=self.fPoint.Y);
 end;
 
-procedure Point2D.Move(const x,y:longint);
+function Point2D.ToString:string;
+begin
+  result:='geom.Point2D'+PointToString(self.fPoint);
+end;
+
+procedure Point2D.MoveTo(const x,y:longint);
 begin
   self.fPoint.X:=x;
   self.fPoint.Y:=y;
 end;
 
-procedure Point2D.Move(const p:Point2D);
+procedure Point2D.MoveTo(const p:Point2D);
 begin
   if p=NIL then
     raise Exception.Create('geom.Point2D.Move(Point): Nil pointer');
@@ -162,6 +265,199 @@ end;
 function Point2D.Distance(const p:Point2D):extended;
 begin
   result:=Point2D.Distance(self,p);
+end;
+
+constructor Rectangle.Create;
+begin
+  inherited Create;
+
+  fRect.left:=0;
+  fRect.top:=0;
+  fRect.right:=0;
+  fRect.bottom:=0;
+end;
+
+constructor Rectangle.Create(const x1,y1,x2,y2:longint);
+begin
+  inherited Create;
+
+  fRect.left:=Min(x1,x2);
+  fRect.top:=Min(y1,y2);
+  fRect.right:=Max(x1,x2);
+  fRect.bottom:=Max(y1,y2);
+end;
+
+constructor Rectangle.Create(const p1,p2:Point2D);
+begin
+  if (p1=NIL) or (p2=NIL) then
+    raise Exception.Create('geom.Rectangle.Create(Point2D,Point2D): Nil pointer');
+
+  Create(p1.X,p1.Y,p2.X,p2.Y);
+end;
+
+constructor Rectangle.Create(const origin:Point2D; const size:Dimension2D);
+begin
+  if (origin=NIL) or (size=NIL) then
+    raise Exception.Create('geom.Rectangle.Create(Point2D,Dimension2D): Nil pointer');
+
+  Create(origin.X,origin.Y,origin.X+size.Width,origin.Y+size.Height);
+end;
+
+constructor Rectangle.Create(const cx,cy:longint);
+begin
+  Create(0,0,cx,cy);
+end;
+
+constructor Rectangle.Create(const size:Dimension2D);
+begin
+  if size=NIL then
+    raise Exception.Create('geom.Rectangle.Create(Dimension2D): Nil pointer');
+
+  Create(size.fSize.cx,size.fSize.cy);
+end;
+
+constructor Rectangle.Create(const p:Point2D);
+begin
+  if p=NIL then
+    raise Exception.Create('geom.Rectangle.Create(Point2D): Nil pointer');
+
+  Create(p,Dimension2D.Create(0,0));
+end;
+
+function Rectangle.Equals(obj:TObject):boolean;
+begin
+  if obj=NIL then
+    raise Exception.Create('geom.Rectangle.Equals(TObject): Nil pointer');
+
+  if obj.ClassType<>self.ClassType then
+    result:=FALSE
+  else
+    result:=self.TopLeft.Equals((obj as Rectangle).TopLeft) and self.BottomRight.Equals((obj as Rectangle).BottomRight);
+end;
+
+function Rectangle.ToString:string;
+begin
+  result:='geom.Rectangle'+RectToString(self.fRect);
+end;
+
+function Rectangle.GetTopLeft:Point2D;
+begin
+  result:=Point2D.Create(left,top);
+end;
+
+function Rectangle.GetBottomLeft:Point2D;
+begin
+  result:=Point2D.Create(left,bottom);
+end;
+
+function Rectangle.GetTopRight:Point2D;
+begin
+  result:=Point2D.Create(right,top);
+end;
+
+function Rectangle.GetBottomRight:Point2D;
+begin
+  result:=Point2D.Create(right,bottom);
+end;
+
+function Rectangle.GetSize:Dimension2D;
+begin
+  result:=Dimension2D.Create(right-left,bottom-top);
+end;
+
+function Rectangle.GetDiagonalLength:extended;
+begin
+  result:=Point2D.Distance(TopLeft,BottomRight);
+end;
+
+function Rectangle.HasWidth:boolean;
+begin
+  result:=not(Left=Right);
+end;
+
+function Rectangle.HasHeight:boolean;
+begin
+  result:=not(Top=Bottom);
+end;
+
+function Rectangle.IsEmpty:boolean;
+begin
+  result:=(Left=Right) or (Top=Bottom);
+end;
+
+function Rectangle.IsSquare:boolean;
+begin
+  result:=(Right-Left)*(Right-Left)=(Bottom-Top)*(Bottom-Top);
+end;
+
+procedure Rectangle.Inflate(const dx,dy:integer);
+begin
+  self.fRect.left:=self.fRect.left-dx;
+  self.fRect.right:=self.fRect.right+dx;
+  self.fRect.top:=self.fRect.top-dy;
+  self.fRect.bottom:=self.fRect.bottom+dy;
+end;
+
+procedure Rectangle.Resize(const newWidth,newHeight:integer);
+begin
+  self.fRect.right:=self.fRect.left+newWidth;
+  self.fRect.bottom:=self.fRect.top+newHeight;
+  //não fazemos nenhuma verificação quanto a inversão left-right e top-bottom... use com responsabilidade!
+end;
+
+procedure Rectangle.Resize(const newSize:Dimension2D);
+begin
+  if newSize=NIL then
+    raise Exception.Create('geom.Rectangle.Resize(Dimension2D): Nil pointer');
+
+  self.Resize(newSize.Width,newSize.Height);
+end;
+
+procedure Rectangle.Offset(const dx,dy:integer);
+begin
+  self.fRect.left:=self.fRect.left+dx;
+  self.fRect.right:=self.fRect.right+dx;
+  self.fRect.top:=self.fRect.top+dy;
+  self.fRect.bottom:=self.fRect.bottom+dy;
+end;
+
+procedure Rectangle.MoveTo(const x,y:integer);
+var
+  sz:Dimension2D;
+begin
+  sz:=self.GetSize;
+  self.fRect.left:=x;
+  self.fRect.top:=y;
+  self.fRect.right:=x+sz.Width;
+  self.fRect.bottom:=y+sz.Height;
+end;
+
+procedure Rectangle.MoveTo(const p:Point2D);
+begin
+  if p=NIL then
+    raise Exception.Create('geom.Rectangle.MoveTo(Point2D): Nil pointer');
+
+  self.MoveTo(p.X,p.Y);
+end;
+
+function Rectangle.PtInRect(const x,y:integer):boolean;
+begin
+  result:=
+    (x>=self.fRect.left)
+    and
+    (x<self.fRect.right)
+    and
+    (y>=self.fRect.top)
+    and
+    (y<self.fRect.bottom);
+end;
+
+function Rectangle.PtInRect(const p:Point2D):boolean;
+begin
+  if p=NIL then
+    raise Exception.Create('geom.Rectangle.PtInRect(Point2D): Nil pointer');
+
+  self.PtInRect(p.X,p.Y);
 end;
 
 end.
